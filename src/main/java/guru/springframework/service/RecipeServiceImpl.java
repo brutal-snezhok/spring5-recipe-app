34c67f9de912137b226b71fa9583c58348944ee9
brutal-snezhok <czyrulik97@mail.ru>
2020-01-13 12:49:43 +0300

    added lombok

--- src/main/java/guru/springframework/service/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/service/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package guru.springframework.service;
 
 import guru.springframework.domain.Recipe;
 import guru.springframework.repository.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
     private final RecipeRepository recipeRepository;
@@ -17,9 +19,10 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in service");
         Set<Recipe> recipeSet = new HashSet<>();
-
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
+
         return recipeSet;
     }
 }
