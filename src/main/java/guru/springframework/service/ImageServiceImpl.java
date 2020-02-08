ada928345bdc7eaff6a37fad61a19ddd1a7b2aab
brutal-snezhok <czyrulik97@mail.ru>
2020-02-08 17:24:15 +0300

    added support to persist images in db

--- src/main/java/guru/springframework/service/ImageServiceImpl.java
+++ src/main/java/guru/springframework/service/ImageServiceImpl.java
@@ -1,17 +1,48 @@
 package guru.springframework.service;
 
+import guru.springframework.domain.Recipe;
+import guru.springframework.repository.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.transaction.Transactional;
+import java.io.IOException;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
 
-    @Override
-    public void saveImageFile(Long recipeId, MultipartFile file) {
+    private final RecipeRepository recipeRepository;
 
-        log.debug("Received a file");
+    public ImageServiceImpl(RecipeRepository recipeService) {
+
+        this.recipeRepository = recipeService;
+    }
+
+    @Override
+    @Transactional
+    public void saveImageFile(Long recipeId, MultipartFile file) {
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()){
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            //todo handle better
+            log.error("Error occurred", e);
+
+            e.printStackTrace();
+        }
 
     }
 }
