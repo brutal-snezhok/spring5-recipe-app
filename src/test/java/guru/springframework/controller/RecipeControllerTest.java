0945419afd3daa4fb1ec29aa01f9d13bc35e8723
brutal-snezhok <czyrulik97@mail.ru>
2020-02-08 22:58:22 +0300

    added handling number format exception

--- src/test/java/guru/springframework/controller/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controller/RecipeControllerTest.java
@@ -84,4 +84,13 @@ public class RecipeControllerTest {
                 .andExpect(status().isNotFound())
                 .andExpect(view().name("404error"));;
     }
+
+    @Test
+    public void testGetRecipeNumberFormatException() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/recipe/asdf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
 }
